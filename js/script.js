71d3706fd27c36107d4d982a15bd98af1b9874a5
Spencer York <spence1534@gmail.com>
2014-04-08 23:14:51 -0500

    Personal: Time at work categorized. Commit 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-alert("hello World");
+//Calculator for how many tickets and calls i did today
+//for complexity ill have it ask me my average time per ticket
+//and the average time per call
+//it will then do some basic math to determine
+//how much time i spent doing each
+
+var userName;
+//username is the variable for the name of the person entering in the information
+
+userName = prompt("User Name: ");
+alert("Hello "+userName);
+//displays the output in the console
+console.log(userName);
+
+//next ill have the program ask for the amount of calls i took
+
+var totalCalls; //variable for the total amount of calls taken
+
+totalCalls = prompt("Total calls taken? ");
+//asks the user the total amount of calls taken
+alert("User: "+userName+" Calls Taken "+totalCalls);
+
+console.log(totalCalls);
+
+var totalTickets;
+totalTickets = prompt("How many tickets have you resolved? ");
+
+alert(userName+" has taken "+totalCalls+" calls & "+"has resolved "+totalTickets+" tickets total.");
+
+console.log(totalTickets);
+
+
