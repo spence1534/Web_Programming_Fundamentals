7e5b0a42b76f62d44f91160ad9701ab4db75045e
Spencer York <spence1534@gmail.com>
2014-04-10 00:52:19 -0500

    Created first files in new project
    added .js and .html files and put simple structure in them.

---
+++ Personal/js/script.js
@@ -0,0 +1,6 @@
+//This will be a program using arrays to calculate a budget and show how much money is
+// left of a check at the end of each week
+
+var budgetArray = Array();
+
+budgetArray [0] = prompt("How many times do you get paid during the month?");
