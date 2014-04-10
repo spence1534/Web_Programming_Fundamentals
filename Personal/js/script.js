f9d588714864412a8a299d9e0035abd3c6344ac9
Spencer York <spence1534@gmail.com>
2014-04-10 01:11:55 -0500

    Added array now debugging

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -3,4 +3,17 @@
 
 var budgetArray = Array();
 
-budgetArray [0] = prompt("How many times do you get paid during the month?");
+budgetArray [0] = prompt("How many times do you get paid during the month?\n" + str.italics("note: please use numbers"));
+
+console.log(budgetArray[0] + " :Amount of times paid");
+
+budgetArray [1] = prompt("What is your average check amount");
+
+console.log(budgetArray[1] + " :Average Check Amount");
+
+
+
+
+
+
+
